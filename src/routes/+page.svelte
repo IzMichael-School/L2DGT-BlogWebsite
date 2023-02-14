b4509c16b9baf3b9c8640f7a03e8ac7866f4dd97
IzMichael <michael@izmichael.com>
2023-02-14 13:07:12 +1300

    Import SvelteKit Template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+    <title>Svelte App</title>
+</svelte:head>
+
+<script>
+    export let data;
+
+    import Header from '$lib/Header.svelte';
+    import Footer from '$lib/Footer.svelte';
+
+    import { auth } from '$lib/stores.js';
+</script>
+
+<Header />
+
+<Footer />
+
+<style>
+    
+</style>
