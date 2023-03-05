805164543428e58f5ddf1607b23fb41bd74ee23b
IzMichael <michael@izmichael.com>
2023-03-06 10:36:15 +1300

    Define types

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,7 +1,9 @@
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
+import type { Anchor } from '$lib/types';
 import PocketBase from 'pocketbase';
 
 export const pb: PocketBase = new PocketBase('https://l2dgt-blog-db.school.izmichael.com');
 
 export const darkmode: Writable<boolean> = writable(false);
+export const anchors: Writable<Anchor[]> = writable([]);
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,8 +2,18 @@ export interface Post {
     id: string,
     slug: string,
     title: string,
+    content: string,
+    snippet: string,
     publishedAt: string,
-    content: string
+    category: string,
+    image: URL,
+    imageAlt: string,
+    imageCaption: string,
+    author: string,
+    expand: {
+        category: Category,
+        author: DBUser
+    }
 };
 
 export interface Category {
@@ -13,9 +23,32 @@ export interface Category {
     path: string
 };
 
+export interface DBUser {
+    id: string,
+    username: string,
+    email: string,
+    created: Date
+};
+
+export interface Comment {
+    id: string,
+    content: string,
+    reader: string,
+    post: Post,
+    expand: {
+        reader: DBUser
+    }
+};
+
 export interface LoadObject {
     params: {
         slug?: string,
         category?: string
     }
 };
+
+export interface Anchor {
+    name: string,
+    id: string,
+    heading: HTMLElement
+}
