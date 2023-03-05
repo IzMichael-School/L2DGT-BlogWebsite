9bc70a98b564ddf6b4093c76841a4657d5091847
IzMichael <michael@izmichael.com>
2023-03-06 10:38:48 +1300

    Add Title Tags to Images

--- src/lib/ListedPost.svelte
+++ src/lib/ListedPost.svelte
@@ -12,10 +12,10 @@
 </script>
 
 <a href="/p/{post.slug}" class="p-2 bg-slate-200 w-full rounded-lg" on:mouseover={() => dispatch('select')} on:focus={() => dispatch('select')}>
-    <img src="https://l2dgt-blog-db.school.izmichael.com/api/files/posts/{post.id}/{post.image}" class="float-left h-20 aspect-video mr-3" alt={post.imageAlt} />
+    <img src="https://l2dgt-blog-db.school.izmichael.com/api/files/posts/{post.id}/{post.image}" class="float-left rounded-lg h-20 aspect-video mr-3" title={post.imageAlt} alt={post.imageAlt} />
     <h2 class="text-lg font-bold h-8 flex flex-row justify-start items-center">{post.title}</h2>
     <h3 class="h-6 flex flex-row justify-start items-center w-2/3 text-left">By {post.expand.author.username} - {post.expand.category.name}</h3>
-    <h3 class="h-6 flex flex-row justify-start items-center w-2/3 text-left">{dayjs(post.publishedAt).format('MMM Do YYYY - HH:mm a')}</h3>
+    <h3 class="h-6 flex flex-row justify-start items-center w-2/3 text-left">{dayjs(post.publishedAt).format('MMM Do, YYYY - HH:mm a')}</h3>
 </a>
 
 <style>
