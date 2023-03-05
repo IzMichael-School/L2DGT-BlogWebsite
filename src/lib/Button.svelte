239e5cf23a5c9dba0c2f9b94a68672ebc855a942
IzMichael <michael@izmichael.com>
2023-03-06 10:38:19 +1300

    Create sliding button component

---
+++ src/lib/Button.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    export let label: string, click: () => void;
+</script>
+
+<button class="btn p-2 hover:bg-blue-500 rounded-lg w-full text-left" on:click={click}>{label}</button>
+
+<style>
+    .btn {
+        position: relative;
+        display: block;
+        overflow: hidden;
+        max-width: 100%;
+        color: #000000;
+        z-index: 1;
+
+        transition-property: all;
+        transition-duration: 300ms;
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    }
+    
+    .btn:hover {
+        color: #ffffff;
+    }
+
+    .btn:before {
+        content: '';
+        position: absolute;
+        inset: 0;
+        background-color: #1B998B;
+        transform: translateX(-100%);
+        transition-property: all;
+        transition-duration: 500ms;
+        transition-timing-function: ease-in-out;
+        z-index: -1;
+    }
+
+    .btn:hover:before {
+        transform: translateX(0);
+    }
+</style>
+
