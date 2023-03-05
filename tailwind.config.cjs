84483f032d7cebc90dfb354ff175c7dc4cf6e260
IzMichael <michael@izmichael.com>
2023-03-06 10:33:45 +1300

    Add Colours, Scrollbar Styling, Font, and Fluidity tweaks

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'salt': '#FAFAFA',
+                'grey': '#8B929E',
+                'navy': '#1B2A41',
+                'red': '#984447',
+                'green': '#1B998B',
+                'blue': '#6A7FDB'
+            }
+        },
         fontFamily: {
             sans: ['Nunito Sans'],
             round: ['Nunito'],
