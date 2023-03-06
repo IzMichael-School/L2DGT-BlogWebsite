be9fe480ecce98248910689c7157043fbe1fdb07
IzMichael <michael@izmichael.com>
2023-03-07 12:34:01 +1300

    Add Sorting, Searching, to Category View

--- src/routes/c/[category]/+page.server.ts
+++ src/routes/c/[category]/+page.server.ts
@@ -1,15 +1,44 @@
 import { pb } from '$lib/stores';
-import type { Post, LoadObject } from '$lib/types';
+import type { Post, LoadObject, Category } from '$lib/types';
+import { error } from '@sveltejs/kit';
 import dayjs from 'dayjs';
 
 export async function load({ params }: LoadObject) {
-    const postlist = await pb.collection('posts').getFullList<Post>(400, {
-        filter: `publishedAt < "${dayjs().format('YYYY-MM-DD HH:mm:ss')}" && category.path = "${params.category}"`,
+    const start = new Date();
+    console.log('1');
+    const postlist = await pb.collection('posts').getFullList<Post>(10, {
+        filter: `publishedAt < "${dayjs().format('YYYY-MM-DD HH:mm:ss')}" && category.slug = "${params?.category}"`,
         expand: 'category,author'
     });
+    console.log('2');
     const posts: Post[] = postlist.map(a => { return JSON.parse(JSON.stringify(a)); });
-    console.log(posts);
-    return {
-        posts: [...posts]
+    console.log('3');
+    const totalReq = await pb.collection('posts').getList(1, 1, {
+        filter: `publishedAt < "${dayjs().format('YYYY-MM-DD HH:mm:ss')}" && category.slug = "${params?.category}"`
+    });
+    console.log('4');
+
+    const categorymatches = await pb.collection('categories').getFullList<Category>(2, {
+        filter: `slug = "${params?.category}"`
+    });
+    console.log('5');
+
+    const category = categorymatches[0];
+    
+    console.log('6');
+    if (category?.name?.length > 0) {
+        console.log('7');
+        console.log('Loaded in', new Date().getTime() - start.getTime(), 'ms');
+        return {
+            posts: [...posts],
+            category: JSON.parse(JSON.stringify(category)),
+            total: totalReq.totalItems
+        };
+    } else {
+        console.log('8');
+        throw error(404, JSON.stringify({
+            detail: 'Category link is invalid',
+            suggestion: 'Try checking the spelling in the link, or <a href="/">Return Home</a>.'
+        }));
     };
 };
--- src/routes/c/[category]/+page.svelte
+++ src/routes/c/[category]/+page.svelte
@@ -1,3 +1,7 @@
+<svelte:head>
+    <title>Iz's Devlog: {data.category.name}</title>
+</svelte:head>
+
 <script lang="ts">
     import Button from '$lib/Button.svelte';
 
@@ -8,48 +12,125 @@
     import ListedPost from '$lib/ListedPost.svelte';
     import sanitizeHtml from 'sanitize-html';
 
+    import '/src/gfm.css';
+
     import { anchors } from '$lib/stores';
     $anchors = [];
 
-    import type { Post } from '$lib/types';
+    import type { Category, Post } from '$lib/types';
     import { goto } from '$app/navigation';
     export let data: DBdata;
     
     interface DBdata {
-        posts: Post[]
+        posts: Post[],
+        category: Category,
+        total: number
     };
     let selected: Post = data.posts[0];
     $: if (data.posts) {
+        data.posts = data.posts.sort((a, b) => a.publishedAt < b.publishedAt ? 1 : -1);
         selected = data.posts[0];
     };
+
+    let search = '', sortOption = 'Date (Desc.)', hoverEnabled = true;
 </script>
 
-<div class="w-full h-full max-h-full overflow-hidden flex flex-row justify-center items-center p-10">
-    <div class="flex flex-col justify-start items-center flex-1 mr-5 h-full max-w-[50%] bg-slate-200 rounded-lg shadow-lg px-5 pt-5 pb-2">
+<div class="flex flex-row items-center justify-center w-full h-full max-h-full p-10 overflow-hidden">
+    <div class="flex flex-col justify-start items-center overflow-hidden flex-1 mr-5 h-full max-w-[50%] bg-slate-200 rounded-lg shadow-lg p-2">
         {#if selected?.id}
-            <img src="https://l2dgt-blog-db.school.izmichael.com/api/files/posts/{selected.id}/{selected.image}" class="rounded-lg" title={selected.imageAlt} alt={selected.imageAlt} />
+            <div class="flex flex-col items-center justify-start flex-1 w-full max-h-full p-3 overflow-hidden text-left">
+                <img src="https://l2dgt-blog-db.school.izmichael.com/api/files/posts/{selected.id}/{selected.image}" class="rounded-lg" title={selected.imageAlt} alt={selected.imageAlt} />
+                
+                <h2 class="w-full mt-5 text-2xl font-bold">{selected.title}</h2>
+                <h3 class="w-full text-lg font-medium">By {selected.expand.author.username} - {selected.expand.category.name}</h3>
+                <h3 class="w-full mb-5 text-lg font-medium">{dayjs(selected.publishedAt).format('MMM Do, YYYY - HH:mm a')}</h3>
             
-            <h2 class="w-full text-left text-2xl font-bold mt-5">{selected.title}</h2>
-            <h3 class="h-6 flex flex-row justify-start items-center w-full text-left text-lg mt-1">By {selected.expand.author.username} - {selected.expand.category.name}</h3>
-            <h3 class="h-6 flex flex-row justify-start items-center w-full text-left text-lg mt-1">{dayjs(selected.publishedAt).format('MMM Do, YYYY - HH:mm a')}</h3>
-        
-            <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-            <p class="w-full text-left flex-1 wrap-long mb-2 overflow-ellipsis whitespace-nowrap overflow-hidden mt-3 max-h-full">{@html sanitizeHtml(selected.content)}</p>
-            <Button label="Read the Post ->" click={() => goto('/p/' + selected.slug)} />
+                <div id="previewContent" class="markdown-body overflow-ellipsis flex-1 w-full overflow-hidden">
+                    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+                    {@html selected.content}
+                </div>
+            </div>
+            
+            <Button label="Read the Post ->" click={() => goto('/p/' + selected.slug)} color={data.category.color} />
         {/if}
     </div>
 
-    <div class="flex flex-col justify-start items-center flex-1 h-full gap-3 max-h-full overflow-y-scroll pr-2">
-        <div class="bg-white sticky top-0 w-full pb-2 -mb-1">
-            <input class="p-2 rounded-lg w-full bg-gray-100 border-2 border-gray-600" placeholder="Search for a Post..." type="text" />
+    <div class="flex flex-col items-center justify-start flex-1 h-full max-h-full gap-3 pr-2 overflow-y-scroll">
+        <div class="bg-salt sticky top-0 w-full pb-2 -mb-1">
+            <label for="search" class="h-11 flex flex-row items-center justify-start w-full overflow-hidden bg-gray-100 border-2 border-gray-600 rounded-lg">
+                <img src="/assets/icons/search.svg" alt="Search Icon" class="aspect-square w-6 h-6 ml-2" />
+                <input id="search" class="flex-1 p-2 bg-gray-100" placeholder="Search for a Post..." type="text" bind:value={search} spellcheck="false" />
+                <button class="{search?.length > 0 ? 'w-7' : 'w-0'} hover:bg-gray-200 h-full overflow-hidden cursor-pointer" on:click={() => search = ''}>
+                    <img src="/assets/icons/cross.svg" alt="Cross Icon" class="aspect-square w-5 h-5 mx-1 overflow-hidden cursor-pointer" />
+                </button>
+            </label>
+            <div class="flex flex-row items-center justify-center w-full mt-1 text-sm text-gray-600">
+                <p class="flex-1 text-left">{data.total} Posts</p>
+
+                <button class="flex flex-row items-center justify-end flex-1 cursor-pointer" on:click={() => {
+                    let options = [ 'Date (Asc.)', 'Date (Desc.)', 'Title (Asc.)', 'Title (Desc.)' ], index = options.indexOf(sortOption) + 1;
+                    if (index >= options.length) index = 0;
+                    sortOption = options[index];
+
+                    switch (sortOption) {
+                    case 'Date (Asc.)':
+                        data.posts = data.posts.sort((a, b) => a.publishedAt > b.publishedAt ? 1 : -1);
+                        break;
+
+                    case 'Date (Desc.)':
+                        data.posts = data.posts.sort((a, b) => a.publishedAt < b.publishedAt ? 1 : -1);
+                        break;
+
+                    case 'Title (Asc.)':
+                        data.posts = data.posts.sort((a, b) => a.title > b.title ? 1 : -1);
+                        break;
+
+                    case 'Title (Desc.)':
+                        data.posts = data.posts.sort((a, b) => a.title < b.title ? 1 : -1);
+                        break;
+
+                    default:
+                        break;
+                    };
+                }}>
+                    <img src="/assets/icons/arrow.svg" alt="Arrow Icon" class="w-5 h-5 invert-[35%] {sortOption.endsWith('(Asc.)') ? 'rotate-0' : 'rotate-180'}" />
+                    <p class="ml-1 text-right"><b>Sort:</b> {sortOption}</p>
+                </button>
+            </div>
         </div>
-        {#each data.posts as post}
-            <ListedPost {post} on:select={() => selected = post} />
+        {#each data.posts.filter(a => a.title.toLowerCase().includes(search?.toLowerCase())) as post}
+            <ListedPost {post} on:select={() => { if (hoverEnabled) selected = post; }} on:click={() => {
+                if (selected.id == post.id) {
+                    hoverEnabled = !hoverEnabled;
+                } else if (!hoverEnabled) {
+                    selected = post;
+                } else {
+                    hoverEnabled = false;
+                };
+            }} selected={!hoverEnabled && selected.id == post.id} />
         {/each}
     </div>
 </div>
 
 
 <style>
-    
+    * {
+        outline: none;
+    }
+
+    #previewContent {
+        position: relative;
+    }
+
+    #previewContent:after {
+        content: "";
+        position: absolute;
+        z-index: 1;
+        bottom: 0;
+        left: 0;
+        pointer-events: none;
+        background: linear-gradient(to bottom, rgba(226, 232, 240, 0), rgba(226, 232, 240, 0.5) 75%, rgba(226, 232, 240, 1) 100%);
+        width: 100%;
+        height: 100%;
+    }
 </style>
