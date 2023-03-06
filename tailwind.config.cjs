fb0d176592b6f0c62edb8666bc6b84da72e3cc00
IzMichael <michael@izmichael.com>
2023-03-07 12:29:40 +1300

    Add "Inter" Font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,16 +13,8 @@ module.exports = {
             }
         },
         fontFamily: {
-            sans: ['Nunito Sans'],
-            round: ['Nunito'],
-            work: ['Work Sans'],
-            mono: ['Overpass Mono'],
-            code: ['Red Hat Mono'],
-            serif: ['PT Serif'],
-            stnd: ['Roboto'],
-            hand: ['Patrick Hand'],
-            loop: ['Sacramento'],
-            clean: ['Outfit'],
+            sans: ['IBM Plex Sans', 'sans-serif'],
+            inter: ['Inter', 'sans-serif']
         },
     },
     plugins: [],
