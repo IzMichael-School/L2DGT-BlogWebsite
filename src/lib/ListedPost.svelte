16af81b8c107d336e12b05436ffa7bced8cc57e8
IzMichael <michael@izmichael.com>
2023-03-07 12:30:49 +1300

    Sort Tailwind Classes

--- src/lib/ListedPost.svelte
+++ src/lib/ListedPost.svelte
@@ -6,17 +6,18 @@
     import { createEventDispatcher } from 'svelte';
 
     import type { Post } from '$lib/types';
-    export let post: Post;
+    export let post: Post, selected = false;
 
     const dispatch = createEventDispatcher();
 </script>
 
-<a href="/p/{post.slug}" class="p-2 bg-slate-200 w-full rounded-lg" on:mouseover={() => dispatch('select')} on:focus={() => dispatch('select')}>
-    <img src="https://l2dgt-blog-db.school.izmichael.com/api/files/posts/{post.id}/{post.image}" class="float-left rounded-lg h-20 aspect-video mr-3" title={post.imageAlt} alt={post.imageAlt} />
-    <h2 class="text-lg font-bold h-8 flex flex-row justify-start items-center">{post.title}</h2>
-    <h3 class="h-6 flex flex-row justify-start items-center w-2/3 text-left">By {post.expand.author.username} - {post.expand.category.name}</h3>
-    <h3 class="h-6 flex flex-row justify-start items-center w-2/3 text-left">{dayjs(post.publishedAt).format('MMM Do, YYYY - HH:mm a')}</h3>
-</a>
+<button class="{selected ? 'bg-slate-300' : 'bg-slate-200'} w-full p-2 rounded-lg cursor-pointer" on:mouseover={() => dispatch('select')} on:focus={() => dispatch('select')} on:click={() => dispatch('click')}>
+    <img src="https://l2dgt-blog-db.school.izmichael.com/api/files/posts/{post.id}/{post.image}" class="aspect-video float-left h-20 mr-3 rounded-lg" title={post.imageAlt} alt={post.imageAlt} />
+    
+    <h2 class="flex flex-row items-center justify-start h-8 text-lg font-bold">{post.title}</h2>
+    <h3 class="flex flex-row items-center justify-start w-2/3 h-6 text-left">By {post.expand.author.username} - {post.expand.category.name}</h3>
+    <h3 class="flex flex-row items-center justify-start w-2/3 h-6 text-left">{dayjs(post.publishedAt).format('MMM Do, YYYY - HH:mm a')}</h3>
+</button>
 
 <style>
     
