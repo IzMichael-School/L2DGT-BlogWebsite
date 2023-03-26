fe666ab12ce90ac4e5577eb02b8087bbfcb2520f
IzMichael <michael@izmichael.com>
2023-03-27 11:36:15 +1300

    Tweak header tab colours

--- src/lib/HeaderTab.svelte
+++ src/lib/HeaderTab.svelte
@@ -5,7 +5,7 @@
     export let title: string, color: string, url: string;
 </script>
 
-<button class="rounded-t-xl min-w-fit self-end flex-1 h-12 p-2 px-10 ml-4 font-bold {$page.url.pathname == url ? 'shadow-inner' : ''}" style="background-color: {$page.url.pathname == url ? 'rgb(241, 245, 249)' : color}; color: {$page.url.pathname == url ? '#000000' : invertColor(color)};" on:click={() => {
+<button class="rounded-t-xl min-w-fit self-end flex-1 h-12 p-2 px-10 ml-4 font-bold {$page.url.pathname == url ? 'shadow-inner' : ''}" style="background-color: {$page.url.pathname == url ? 'rgb(231, 235, 239)' : color}; color: {$page.url.pathname == url ? '#000000' : invertColor(color)};" on:click={() => {
     goto(url);
 }}>{title}</button>
 <!-- <a class="rounded-t-xl min-w-fit self-end flex-1 h-12 p-2 px-10 ml-4 font-bold {$page.url.pathname == url ? 'shadow-inner' : ''}" style="background-color: {$page.url.pathname == url ? 'rgb(241, 245, 249)' : color}; color: {$page.url.pathname == url ? '#000000' : invertColor(color)};" href={url}>{title}</a> -->
